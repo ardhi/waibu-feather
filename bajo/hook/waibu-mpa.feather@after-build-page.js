0e34201972d84c437370cfdaf6063ad7a7e95170
Ardhi Lukianto <ardhi@lukianto.com>
2024-08-06 10:39:04 +0700

    Now handled directly on iconset itself

--- bajo/hook/waibu-mpa.feather@after-build-page.js
+++
@@ -1,9 +0,0 @@
-async function waibuMpaFeatherAfterBuildPage ({ $ }) {
-  const inline = `
-<script>
-  feather.replace()
-</script>`
-  $('body').append(inline)
-}
-
-export default waibuMpaFeatherAfterBuildPage
